c934f20e64f9fb7e4d450b161a846d7628d2c758
ggh <87795954+heieirk@users.noreply.github.com>
2024-01-18 23:37:42 +0900

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: '/bare/',
+    bare: 'https://kaguya1-5u937pco.b4a.run/bare/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
